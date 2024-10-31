6ff6c840a1c47fd4420f7ff00cc1fa26794360ea
Cardoso Ribeiro <vcardosor@minsait.com>
2024-10-30 22:49:21 -0300

    Service Principal implementado

--- main.tf
+++ main.tf
@@ -8,11 +8,13 @@ terraform {
 }
 
 provider "azurerm" {
+  features {
+
+  }
   subscription_id = var.subscription_id
   client_id = var.client_id
   client_secret = var.client_secret
   tenant_id = var.tenant_id
-  features {}
 }
 
 resource "azurerm_resource_group" "rg" {
